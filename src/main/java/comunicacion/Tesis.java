62890318cff8773292797f10ebfd31567e4d95d4
JEROLPOA2 <jledesmap@unal.edu.co>
2022-10-10 19:16:25 -0500

    2

--- src/main/java/comunicacion/Tesis.java
+++ src/main/java/comunicacion/Tesis.java
@@ -31,7 +31,7 @@ public class Tesis extends Escrito{
     @Override
     public String toString() {
        
-        return this.getOrigen() + "\n" + this.getTitulo() + "\n" + this.getAutor() + "\n" + this.getPaginas() + "\n" + this.idea + "\n" + this.argumentos + "\n" + this.conclusion + "\n" + this.referencias;
+        return this.getOrigen() + "\n" + this.getTitulo() + "\n" + this.getAutor() + "\n" + this.getPaginas() + "\n" + this.idea + "\n" + this.argumentos.length + "\n" + this.conclusion + "\n" + this.referencias;
     }
 
     public String getIdea() {
